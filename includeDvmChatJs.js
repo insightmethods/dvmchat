5af8e285c5a1fc3113d2d1626ffceb792116c1ef
Rajanee <rajanee@beehively.com>
2018-02-12 17:42:49 +0530

    Addded json and js file for DVM chat set up

---
+++ includeDvmChatJs.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  $.getJSON('https://raw.githubusercontent.com/insightmethods/dvmchat/master/includeDvmChat.json', function(obj) {
+      if(obj[window.location.hostname])
+      {
+        ;(function(o,l,a,r,k,y){if(o.olark)return;
+        r="script";y=l.createElement(r);r=l.getElementsByTagName(r)[0];
+        y.async=1;y.src="//"+a;r.parentNode.insertBefore(y,r);
+        y=o.olark=function(){k.s.push(arguments);k.t.push(+new Date)};
+        y.extend=function(i,j){y("extend",i,j)};
+        y.identify=function(i){y("identify",k.i=i)};
+        y.configure=function(i,j){y("configure",i,j);k.c[i]=j};
+        k=y._={s:[],t:[+new Date],c:{},l:a};
+        })(window,document,"static.olark.com/jsclient/loader.js");
+        olark.identify('5196-724-10-5701');
+      }
+ });
+});
